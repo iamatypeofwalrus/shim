1d83a39f29f56830d7696d1352faeac166b73955
Joe Feeney <joe@glasswaves.co>
2018-01-15 21:42:57 -0800

    add shim and response writer

---
+++ response_writer.go
@@ -0,0 +1,32 @@
+package shim
+
+import (
+	"bytes"
+	"net/http"
+)
+
+// ResponseWriter adheres to the http.ResponseWriter interface and provides
+// additional methods for integrating with Lambda
+type ResponseWriter struct {
+	Code    int
+	Headers http.Header
+	Body    bytes.Buffer
+}
+
+// Header adheres the http.ResponseWriter interface
+func (rw ResponseWriter) Header() http.Header {
+	return rw.Headers
+}
+
+// Write adheres to the io.Writer interface
+func (rw ResponseWriter) Write(b []byte) (int, error) {
+	if rw.Code == 0 {
+		rw.WriteHeader(http.StatusOK)
+	}
+	return rw.Body.Write(b)
+}
+
+// WriteHeader adheres to the http.ResponseWriter interface
+func (rw ResponseWriter) WriteHeader(c int) {
+	rw.Code = c
+}
---
+++ shim.go
@@ -0,0 +1,77 @@
+package shim
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+const (
+	contentType = "Content-Type"
+)
+
+// New returns an initialized Shim
+func New(h http.Handler) *Shim {
+	return &Shim{
+		Handler: h,
+	}
+}
+
+// Shim provides
+type Shim struct {
+	Handler http.Handler
+}
+
+// Handle converts an APIGatewayProxyRequest in an http.Request, creates a new ResponseWriter,
+// and passes it to its http.Handler
+func (s *Shim) Handle(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	req, err := http.NewRequest(
+		request.HTTPMethod,
+		request.Path,
+		strings.NewReader(request.Body),
+	)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	rw := &ResponseWriter{}
+	s.Handler.ServeHTTP(rw, req)
+
+	headers := FormatHeaders(rw.Headers)
+	SetDefaultContentType(headers, rw.Body.Bytes())
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: rw.Code,
+		Body:       rw.Body.String(),
+		Headers:    headers,
+	}, nil
+}
+
+// SetDefaultContentType attempts to detect and set the Content-Type header for a given response body
+func SetDefaultContentType(lambdaHeaders map[string]string, body []byte) {
+	if _, ok := lambdaHeaders[contentType]; !ok {
+		lambdaHeaders[contentType] = http.DetectContentType(body)
+	}
+}
+
+// FormatHeaders is a convinience function that converts an http.Header into a map[string]string
+func FormatHeaders(h http.Header) map[string]string {
+	headers := make(map[string]string)
+
+	for k, v := range h {
+		// No great options here. Rather than cat-ing the string array together
+		// and on average getting it right, I'd rather just take the first item
+		// in the array.
+		var str string
+		if len(v) > 0 {
+			str = v[0]
+		}
+
+		headers[k] = str
+	}
+
+	return headers
+}
