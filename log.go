27a1d5b2b2b05788481370566530e4207df08e3c
Joe Feeney <joe@glasswaves.co>
2024-04-06 16:36:09 +0000

    simplify logging interface to just printf

--- log.go
+++ log.go
@@ -3,5 +3,4 @@ package shim
 // Log is a simple logging interface that is satisfied by the standard library logger amongst other idiomatic loggers
 type Log interface {
 	Printf(format string, v ...interface{})
-	Println(v ...interface{})
 }
--- shim.go
+++ shim.go
@@ -7,6 +7,11 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 )
 
+// todo:
+// 1. allow slog to be sent in as debug logger, logger requires just one function: printf
+// 2. create two new apis one for RestApi and on for HttpApi
+// 3. have og call RestApi
+
 // Handler is an interface for accepting and responding to API Gateway integration requests
 type Handler interface {
 	Handle(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error)
@@ -50,25 +55,19 @@ func (s *Shim) Handle(ctx context.Context, request events.APIGatewayProxyRequest
 
 	httpReq, err := NewHTTPRequest(ctx, request)
 	if err != nil {
-		s.println("received an error while constructing http request from API Gateway request event")
+		s.printf("received an error while constructing http request from API Gateway request event\n")
 		return events.APIGatewayProxyResponse{}, err
 	}
 	s.printf("http request: %+v", httpReq)
 	rw := NewResponseWriter()
 
-	s.println("calling ServeHTTP on shim handler")
+	s.printf("calling ServeHTTP on shim handler\n")
 	s.Handler.ServeHTTP(rw, httpReq)
 	s.printf("received response: %+v\n", rw)
 
 	resp := NewAPIGatewayProxyResponse(rw)
 	s.printf("api gateway proxy response: %+v\n", resp)
 	return resp, nil
-}
-
-func (s *Shim) println(v ...interface{}) {
-	if s.Log != nil {
-		s.Log.Println(v...)
-	}
 }
 
 func (s *Shim) printf(format string, v ...interface{}) {
